aa5ebb897de630bc34650ad552a4b19ec0cbfa06
vedantpatel02007 <vedantpatel02007@gmail.com>
2025-05-01 12:22:36 +0530

    Add files via upload

---
+++ 2_logical_operators.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int result;
+
+    result = a < b && b < c;
+    printf("&d %d<%d && %d<%d",result,a,b,b,c);
+
+    result = a < b || b > c;
+    printf("\n%d %d<%d && %d<%d",result,a,b,c);
+
+    result = a > b || b > c;
+    printf("\n%d %d>%d && %d<%d",result,a,b,c);
+
+    result = !(a == b);
+    printf("\n%d == %d",result,a,b);
+}

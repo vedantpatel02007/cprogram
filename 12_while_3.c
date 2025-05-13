3f9582be983e577f025943b63edec3dab8c03d5f
vedantpatel02007 <vedantpatel02007@gmail.com>
2025-05-13 12:32:14 +0530

    Add files via upload

---
+++ 12_while_3.c
@@ -0,0 +1,34 @@
+/*
+  1.2) 1 4 9 16 25  ...... 1000
+  steps
+  ------------------------------------
+  1-create variable number
+  2-display number
+  3- number = number*number
+  4-print number
+*/
+#include<stdio.h>
+void main()
+{
+    int number=0;
+    int numb;
+    while(number<=100)
+    {
+       
+        
+        number = number + 1;
+        
+        
+        numb = number*number;
+        printf("%d ",numb);
+        
+
+        
+        
+       
+      
+    }
+   
+ 
+
+}
